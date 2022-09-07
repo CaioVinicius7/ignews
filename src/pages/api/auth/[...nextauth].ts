7adf1abf50ced30ec6bb69603d4fc8307c903967
Caio Vinícius <caio1525pereira@gmail.com>
2022-09-07 01:46:54 -0300

    Verificando usuário duplicado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -22,13 +22,14 @@ export default NextAuth({
       const { email } = user;
       try {
         await fauna.query(
-          q.Create(q.Collection("users"), {
-            data: {
-              email
-            }
-          })
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+          )
         );
-
         return true;
       } catch {
         return false;
